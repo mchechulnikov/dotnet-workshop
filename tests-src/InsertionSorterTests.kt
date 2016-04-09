f22a2e8edadd15121326c495157e82e1659f62b5
Максим Чечульников <ilfirin.nar@gmail.com>
2016-04-09 23:17:51 +0300

    First terrible test

---
+++ tests-src/InsertionSorterTests.kt
@@ -0,0 +1,11 @@
+package sorting.tests
+
+import org.junit.Assert
+import org.junit.Test
+
+class InsertionSorterTests {
+    @Test
+    fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+}
