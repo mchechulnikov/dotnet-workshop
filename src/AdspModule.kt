69f100a758ac991bc88d3dbc461bcb9807a45f43
Максим Чечульников <ilfirin.nar@gmail.com>
2016-04-12 21:11:20 +0300

    Remove dagger

--- src/AdspModule.kt
+++
@@ -1,9 +0,0 @@
-import dagger.Module
-import dagger.Provides
-import sorting.InsertionSorter
-import sorting.Sorter
-
-@Module
-class AdspModule {
-    @Provides fun provideSorter(): Sorter = InsertionSorter()
-}
