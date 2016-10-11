021d5532fbf1870015b7eb78396b8577d476d917
sunloving <ilfirin.nar@gmail.com>
2016-10-12 00:30:59 +0300

    merge sort (@)

--- src/dotnet/adsp/Adsp.Algorithms.Tests/Sorting/MergeSorterTests.cs
+++ src/dotnet/adsp/Adsp.Algorithms.Tests/Sorting/MergeSorterTests.cs
@@ -1,20 +1,27 @@
-﻿using Xunit;
+﻿using System.Collections;
+using Xunit;
+using Xunit.Abstractions;
 using Adsp.Algorithms.Sorting;
 
 namespace Adsp.Algorithms.Tests.Sorting
 {
     public class MergeSorterTests
     {
-        [Fact]
-        public void Sort_Valid()
-        {
-            var array = new [] { 42, 22, 46, 2, 33, 3, 7574 };
-            var sortedArray = MergeSorter.Sort(array);
+      [Fact]
+      public void Sort_UnsortedArray_Valid()
+      {
+        var array = new [] { 42, 22, 46, 2, 33, 3, 7574 };
+        var sortedArray = MergeSorter.Sort(array);
 
-            foreach (var item in sortedArray)
-            {
-                Assert.True(array.Contains(item));
-            }
-        }
+        Assert.Equal(2, sortedArray[0]);
+      }
+
+      [Fact]
+      public void Sort_UnsortedArray_EqualLength()
+      {
+        var array = new [] { 42, 22, 46, 2, 33, 3, 7574 };
+        var sortedArray = MergeSorter.Sort(array);
+        Assert.Equal(array.Length, sortedArray.Length);
+      }
     }
 }
