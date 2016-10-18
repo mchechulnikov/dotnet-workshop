7dc901045f0c0a0fd07c6ff63892cd07fd4648e4
sunloving <ilfirin.nar@gmail.com>
2016-10-18 22:39:40 +0300

    start impl binary heap

---
+++ src/dotnet/adsp/Adsp.DataStructs.Tests/BinaryHeapTests.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Adsp.DataStructs.Tests
+{
+  public class BinaryHeapTests
+  {
+    public static void Main(string[] args)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ src/dotnet/adsp/Adsp.DataStructs/BinaryHeap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Adsp.DataStructs
+{
+  public class BinaryHeap
+  {
+    private readonly int[] _array;
+    
+    public BinaryHeap(_array)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
