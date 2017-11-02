b507c03bf26a80f79d86a5da923561ec56b5e899
Maxim Chechulnikov <ilfirin.nar@gmail.com>
2017-11-02 12:59:26 +0300

    Create MaxDiff.cs

---
+++ Tasks/MaxDiff.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks
+{
+    public class MaxDiff
+    {
+        public static int Calc(int[] a)
+        {
+            var maxDiff = -1;
+            var max = a[a.Length - 1];
+            for (var i = a.Length - 2; i >= 0; i--)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+                else
+                {
+                    var diff = max - a[i];
+                    if (diff > maxDiff)
+                    {
+                        maxDif = diff;
+                    }
+                }
+            }
+            return maxDiff;
+        }
+    }
+}
